5ebf62daf252d8b77cd574110f89823b24236875
joujoucous <coustenoble.julie@gmail.com>
2018-02-02 14:13:39 +0100

    Fix creat script

--- create.sql
+++ create.sql
@@ -1,17 +1,16 @@
-drop table if exists student;
-drop table if exists match;
-drop table if exists message;
-drop table if exists adjective;
-drop table if exists token;
+drop table if exists student CASCADE;
+drop table if exists match CASCADE;
+drop table if exists message CASCADE;
+drop table if exists adjective CASCADE;
+drop table if exists token CASCADE;
 
 create table adjective (
-	id_adjective integer not null,
-	wording varchar(30) not null,
-	primary key (id_adjective)
+	id_adjective SERIAL primary key,
+	wording varchar(30) not null
 );
 
 create table student (
-	id_student integer not null,
+	id_student SERIAL primary key,
 	surname varchar(30) not null,
 	email varchar(50) not null,
 	password_student varchar(10) not null,
@@ -23,40 +22,34 @@ create table student (
 	adjective_2 integer not null,
 	adjective_3 integer not null,
 	validate_account boolean default false,
-	primary key (id_student),
 	foreign key (adjective_1) references adjective(id_adjective) ,
 	foreign key (adjective_2) references adjective(id_adjective),
 	foreign key (adjective_3) references adjective(id_adjective)  
 );
 
 create table match (
-	id_match integer not null,
+	id_match SERIAL primary key,
 	result boolean default false, 
 	id_student_god_father integer not null,
 	id_student_god_son integer not null,
-	primary key (id_match),
 	foreign key (id_student_god_father) references Student(id_student),
 	foreign key (id_student_god_son) references Student(id_student)
 );
 
 create table message (
-	id_message integer not null,
+	id_message SERIAL primary key,
 	id_recipient integer not null,
 	date_message date,
 	content text,
 	id_sender integer not null,
-	primary key (id_message),
 	foreign key (id_sender) references Student(id_student)
 );
 
-
-
-	
 create table token (
 	birth date,
 	token varchar(32),
 	is_alive boolean default true,
-	id_student integer not null,
+	id_student integer primary key,
 	foreign key (id_student) references student(id_student)
 );
 	
