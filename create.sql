064059f0bd30e4dc743ccc4b9fe6d94ac03d8242
Alice <alice.capelle@sfr.fr>
2018-02-02 11:56:31 +0100

    Respect naming convention in database create script

--- create.sql
+++ create.sql
@@ -1,61 +1,61 @@
-drop table Student
-drop table Match
-drop table Message
-drop table Adjective
-drop table Token
+drop table if exists student;
+drop table if exists match;
+drop table if exists message;
+drop table if exists adjective;
+drop table if exists token;
 
-create table Student (
-	IdStudent integer not null,
-	SurnameStudent varchar(30) not null,
-	Email varchar(50) not null,
-	PasswordStudent varchar(10) not null,
-	Year integer not null,
-	Pic 
-	Description longtext,
-	Score integer not null,
-	Adjective1 integer not null,
-	Adjective2 integer not null,
-	Adjective3 integer not null,
-	validateAccount boolean default false,
-	primary key (IdStudent),
-	foreign key (Adjective1) references Adjective(IdAdjective) ,
-	foreign key (Adjective2) references Adjective(IdAdjective),
-	foreign key (Adjective3) references Adjective(IdAdjective)  
+create table student (
+	id_student integer not null,
+	surname varchar(30) not null,
+	email varchar(50) not null,
+	`password` varchar(10) not null,
+	year integer not null,
+	pic varchar(30) not null, 
+	description longtext,
+	score integer not null,
+	adjective_1 integer not null,
+	adjective_2 integer not null,
+	adjective_3 integer not null,
+	validate_account boolean default false,
+	primary key (id_student),
+	foreign key (adjective_1) references Adjective(id_adjective) ,
+	foreign key (adjective_2) references Adjective(id_adjective),
+	foreign key (adjective_3) references Adjective(id_adjective)  
 )
 
-create table Match (
-	IdMatch integer not null,
-	Result boolean default false;, 
-	IdStudentGodFather integer not null,
-	IdStudentGodSon integer not null,
-	primary key (IdMatch),
-	foreign key (IdStudentGodFather) references Student(IdStudent),
-	foreign key (IdStudentGodSon) references Student(IdStudent)
+create table match (
+	id_match integer not null,
+	result boolean default false;, 
+	id_student_god_father integer not null,
+	id_student_god_son integer not null,
+	primary key (id_match),
+	foreign key (id_student_god_father) references Student(id_student),
+	foreign key (id_student_god_son) references Student(id_student)
 )
 
-create table Message (
-	IdMessage integer not null,
-	IdRecipient integer not null,
-	DateMessage date,
-	Content longtext,
-	IdSender integer not null,
-	primary key (IdMessage),
-	foreign key (IdSender) references Student(IdStudent)
+create table message (
+	id_message integer not null,
+	id_recipient integer not null,
+	date_message date,
+	content longtext,
+	id_sender integer not null,
+	primary key (id_message),
+	foreign key (id_sender) references Student(id_student)
 )
 
-create table Adjective (
-	IdAdjective integer not null,
-	Wording varchar(30) not null,
-	primary key (IdAdjective)
+create table adjective (
+	id_adjective integer not null,
+	wording varchar(30) not null,
+	primary key (id_adjective)
 )
 	
 	
 create table token (
-	Birth date,
-	Token varchar(32),
-	IsAlive boolean default true,
-	IdStudent integer not null,
-	foreign key (IdStudent) references Student(IdStudent)
+	birth date,
+	token varchar(32),
+	is_alive boolean default true,
+	id_student integer not null,
+	foreign key (id_student) references student(id_student)
 )	
 	
 
