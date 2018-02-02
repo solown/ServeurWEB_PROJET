ec42ae33823c9b8773ef2f9c36d1dd0d56697ebb
raphael <raphael.neveu98@gmail.com>
2018-02-02 10:40:15 +0100

    Added register confirmation page

---
+++ site/register-confirmation.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+
+</head>
+<body>
+    <h1>Merci de votre inscription !</h1>
+        <p>
+            Un e-mail a été envoyé à l'adresse <?php echo $_POST['mail']; ?>@etu.parisdescartes.fr afin de confirmer celle-ci.</br>
+            Vous êtes en année : <?php echo $_POST['year'] ?>
+        </p>
+</body>
+</html>
