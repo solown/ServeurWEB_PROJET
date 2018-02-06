7168a817cc516dd47ed766218336de2e1d44ee38
joujoucous <coustenoble.julie@gmail.com>
2018-02-06 10:35:47 +0100

    correct error on php

--- site/register-confirmation.php
+++ site/register-confirmation.php
@@ -1,19 +1,22 @@
 <!DOCTYPE html>
 
 <html>
+
 <head>
-    <meta charset="utf-8" />
+	<meta charset="utf-8" />
 	<title>Confirmation de votre inscription</title>
 	<link rel="stylesheet" href="styles/style.css">
 	<link href="https://fonts.googleapis.com/css?family=Fjalla+One" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
 </head>
+
 <body style="text-align:center">
-    <h1>Merci de votre inscription !</h1>
-        <p>
-            Un e-mail a été envoyé à l'adresse <?php echo $_POST['mail'];?>@etu.parisdescartes.fr afin de confirmer celle-ci.</br>
-        </p>
-        <?php 
+	<h1>Merci de votre inscription !</h1>
+	<p>
+		Un e-mail a été envoyé à l'adresse
+		<?php echo $_POST['mail'];?>@etu.parisdescartes.fr afin de confirmer celle-ci.</br>
+	</p>
+	<?php 
             $host_db='localhost';
             $name_db='tinder';
             $user_db='tinder';
@@ -24,9 +27,9 @@
             $student_mail = $_POST['mail'];
             $student_pass = $_POST['password'];
             $student_year = $_POST['year'];
-            <?PHP
+            
             $password_hash = crypt($student_pass);
-          ?>
+          
             try{
                 $db = new PDO("pgsql:host=".$host_db.";dbname=".$name_db."", "".$user_db."", "".$pass_db."") or die(print_r($db->errorInfo()));
                 $db->exec("SET NAMES utf8");
@@ -50,4 +53,5 @@
             }
         ?>
 </body>
+
 </html>
