be35a52789ebcd3deba49360c427a142b4bf7dc5
raphael <raphael.neveu98@gmail.com>
2018-02-06 19:54:56 +0100

    Correct indentation and spacing

--- site/identification.php
+++ site/identification.php
@@ -20,38 +20,37 @@
 	</p>
 	<?php 
  
-            $host_db='localhost';
-            $name_db='tinder';
-            $user_db='tinder';
-            $pass_db='tinder';
-  			$password_entered=$_POST['password'];
-   			$student_mail=$_POST['mail'];
-           
+    $host_db='localhost';
+    $name_db='tinder';
+    $user_db='tinder';
+    $pass_db='tinder';
+    $password_entered=$_POST['password'];
+    $student_mail=$_POST['mail'];
 
-            try{
-                $db = new PDO("pgsql:host=".$host_db.";dbname=".$name_db."", "".$user_db."", "".$pass_db."") or die(print_r($db->errorInfo()));
-                $db->exec("SET NAMES utf8");
-                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-                
-                $query = "SELECT password_student FROM STUDENT WHERE email = :mail";
-                $statement = $db->prepare($query);
-                $statement->bindValue(':mail', $student_mail);
-                $statement->execute();
-                $password_hash = $statement->setFetchMode(PDO::FETCH_ASSOC);
-				 echo $password_hash;
-			}
-            
-            catch(Exeption $e){
-                die("Erreur!".$e->getMessage());
-            }
+    try{
+        $db = new PDO("pgsql:host=".$host_db.";dbname=".$name_db."", "".$user_db."", "".$pass_db."") or die(print_r($db->errorInfo()));
+        $db->exec("SET NAMES utf8");
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+        
+        $query = "SELECT password_student FROM STUDENT WHERE email = :mail";
+        $statement = $db->prepare($query);
+        $statement->bindValue(':mail', $student_mail);
+        $statement->execute();
+        $password_hash = $statement->setFetchMode(PDO::FETCH_ASSOC);
+        echo $password_hash;
+    }    
+    catch(Exeption $e){
+        die("Erreur!".$e->getMessage());
+    } 
 
-  if(crypt($password_entered, $password_hash) == $password_hash) {
-    echo  'identification réussie';
-  }else{
-	echo  'ERREUR d identification';
-  }
+    if(crypt($password_entered, $password_hash) == $password_hash) {
+      echo  'identification réussie';
+    }
+    else{
+      echo  'ERREUR d identification';
+    }
 
-        ?>
+  ?>
 </body>
 
 </html>
