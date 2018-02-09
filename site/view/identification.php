74bd1b0b25ee7de3d69d3817b2f56f602cf9bbec
raphael <imgonadaie@gmail.com>
2018-02-09 19:35:07 +0100

    Created new view php files containing the former 'view code' that was in the controller php files

---
+++ site/view/identification.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<meta charset="utf-8" />
+	<title>identification</title>
+	<link rel="stylesheet" href="styles/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Fjalla+One" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
+</head>
+
+<body style="text-align:center">
+
+	<p>
+		<?php Bienvenue echo $_POST['mail'];?>@etu.parisdescartes.fr
+		</br>
+	</p>
+	</body>
+</html>
