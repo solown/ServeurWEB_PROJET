b9cce0fe72e17fd1cff08b893b117ad9aaedd6a6
joujoucous <coustenoble.julie@gmail.com>
2018-02-23 13:54:39 +0100

    control on sign up added

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -1,3 +1,42 @@
- var a = document.getElementById("test");
+function surligne(champ, erreur) {
+	if (erreur)
+		champ.style.backgroundColor = "#fba";
+	else
+		champ.style.backgroundColor = "";
+}
 
- a.style.fontSize = "100px";
+function verifMail(champ) {
+	var regex = /^[a-z]+\.[a-z]+[0-9]*$/;
+	if (!regex.test(champ.value)) {
+		surligne(champ, true);
+		return false;
+	} else {
+		surligne(champ, false);
+		return true;
+	}
+}
+
+
+function verifpassword(champ) {
+	if (champ.value.length < 20) {
+		surligne(champ, true);
+		return false;
+	} else {
+		surligne(champ, false);
+		return true;
+	}
+}
+
+function verifForm(e) {
+
+	var mailOk = verifMail(document.getElementById("mail"));
+	var passwordOK = verifpassword(document.getElementById("password"));
+	console.log(mailOk + passwordOK);
+	if (passwordOK && mailOk)
+		return;
+	else {
+		alert("20 caractères MINIMUM pour le mot de passe et veuillez entrer la partie gauche de votre adresse étudiante");
+		e.preventDefault();
+	}
+
+}
