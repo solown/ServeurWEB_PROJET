2d3a74368dc5d89f0f3b1e0ac11f42ef13d9daaa
raphael <imgonadaie@gmail.com>
2018-03-09 12:26:16 +0100

    Add account confirmation php code - unfinished

--- site/view/loginPerso.php
+++ site/view/loginPerso.php
@@ -11,6 +11,8 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 
+<?php require("../model/confirm-account.php"); ?>
+
 <body>
 	<div class="row fullscreen_height">
 		<div class="col-5 right_part ">
