a524b6fedb3fd7e5a0f0c5bc45e158b0fdc56a51
THIBAUD MAGNE <magne@iut.univ-paris5.fr>
2018-03-09 12:01:51 +0100

    Full page updateprofile. Need few update

---
+++ site/scripts/updateprofile.js
@@ -0,0 +1,17 @@
+var text;
+
+
+function update(){
+	document.getElementsByClassName('resume')[0].style.display = 'none';
+	document.getElementById('inputresume').style.display = 'block';
+	document.getElementsByClassName('buttonupdate')[0].style.display = 'none';
+	document.getElementsByClassName('buttonconfirm')[0].style.display = 'block';
+}
+
+function confirm(){
+	text=document.getElementById('inputresume').value
+	document.getElementsByClassName('resume')[0].value = text;
+	document.getElementById('inputresume').style.display = 'none';
+	document.getElementsByClassName('resume')[0].style.display = 'block';
+	
+}
