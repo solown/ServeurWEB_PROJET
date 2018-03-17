9ba71b78204915937ab16466dcfb0af2931d612b
raphael <imgonadaie@gmail.com>
2018-03-17 19:19:23 +0100

    Move scripts which don't need to be treated by Babel or browserify from scripts-dev to scripts

--- site/scripts/checkForm.js
+++ site/scripts/checkForm.js
@@ -1,8 +1,8 @@
-(function(){function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s}return e})()({1:[function(require,module,exports){
-"use strict";
-
 function highlight(field, error) {
-	if (error) field.style.backgroundColor = "#fba";else field.style.backgroundColor = "";
+	if (error)
+		field.style.backgroundColor = "#fba";
+	else
+		field.style.backgroundColor = "";
 }
 
 function checkMail(field) {
@@ -15,6 +15,7 @@ function checkMail(field) {
 		return true;
 	}
 }
+
 
 function checkPassword(field) {
 	if (field.value.length < 8) {
@@ -31,9 +32,11 @@ function checkForm(e) {
 	var mailOk = checkMail(document.getElementById("mail"));
 	var passwordOK = checkPassword(document.getElementById("password"));
 	console.log(mailOk + passwordOK);
-	if (passwordOK && mailOk) return;else {
+	if (passwordOK && mailOk)
+		return;
+	else {
 		alert("8 caractères MINIMUM pour le mot de passe et veuillez entrer la partie gauche de votre adresse étudiante");
 		e.preventDefault();
 	}
+
 }
-},{}]},{},[1]);
--- site/scripts/swipe.js
+++ site/scripts/swipe.js
@@ -1,19 +1,17 @@
-(function(){function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s}return e})()({1:[function(require,module,exports){
-"use strict";
+const yes = document.querySelector(".yes")
+const swipe_profil = document.querySelector(".swipe_profil")
+const no = document.querySelector(".no")
 
-var yes = document.querySelector(".yes");
-var swipe_profil = document.querySelector(".swipe_profil");
-var no = document.querySelector(".no");
+var recycled = document.querySelector(".bounceOutLeft")
+var hearted = document.querySelector(".bounceOutRight")
 
-var recycled = document.querySelector(".bounceOutLeft");
-var hearted = document.querySelector(".bounceOutRight");
-
-yes.addEventListener("click", function () {
+yes.addEventListener("click", () => {
 	swipe_profil.classList.add("bounceOutRight");
-});
-no.addEventListener("click", function () {
+
+})
+no.addEventListener("click", () => {
 	swipe_profil.classList.add("bounceOutLeft");
-});
+})
 
 function top_back() {
 	swipe_profil.classList.remove("bounceOutRight");
@@ -23,4 +21,3 @@ function top_back() {
 
 swipe_profil.addEventListener("webkitAnimationEnd", top_back, false);
 swipe_profil.addEventListener("animationend", top_back, false);
-},{}]},{},[1]);
