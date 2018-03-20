3f2471886c9aeaba14d3bac008c469000aa946ab
raphael <imgonadaie@gmail.com>
2018-03-20 11:20:25 +0100

    Center input fields on test page

--- site/view/test.php
+++ site/view/test.php
@@ -12,7 +12,8 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	
 	<style>
-		.adj-input { display:block; }
+		.adj-input { display:block;
+					 text-align:center; }
 		.title_logo { margin-top:25%; }
 	</style>
 </head>
