4a83c28b02f556b2ec1aac582b1438ff1ee2c698
raphael <imgonadaie@gmail.com>
2018-03-29 17:14:05 +0200

    Exclude current student in request in model/swipe.php

--- site/model/swipe.php
+++ site/model/swipe.php
@@ -6,9 +6,9 @@ if($db) {
 	
 	$query_get_score = "SELECT score 
 	FROM student
-	WHERE id_student = :id_score";
+	WHERE student_id = :student_id";
 	$statement_score = $db->prepare($query_get_score);
-	$statement_score->bindValue(':id_score', $_SESSION['id']); 
+	$statement_score->bindValue(':id_student', $_SESSION['id']); 
 	$statement_score->execute();
 	
 	while($row = $statement_score->fetch(PDO::FETCH_ASSOC)){
@@ -19,26 +19,19 @@ if($db) {
 	$score_max = $score_student + 50;
 	
 	$query_get_student = "SELECT surname, description
-	FROM student
-	WHERE score BETWEEN :score_min AND :score_max";
+		FROM student
+		WHERE score BETWEEN :score_min AND :score_max AND id_student <> :student_id";
 	$statement_student = $db->prepare($query_get_student);
 	$statement_student->bindValue(':score_min', $score_min, PDO::PARAM_INT);
 	$statement_student->bindValue(':score_max', $score_max, PDO::PARAM_INT);
+	$statement_student->bindValue(':student_id', $SESSION['id']);
 	$statement_student->execute();
 	
 
 	$count = 0;
 	
-	//var_dump($statement_student);
-	
 	while($row = $statement_student->fetch(PDO::FETCH_ASSOC)){	
 		array_push($tab_student, array('name'=>$row['surname'], 'description'=>$row['description']));
 		$count=$count+1;
 	}
-
-	for($i = 0; $i < count($tab_student); $i++){
-		var_dump($tab_student[$i]);
-	}
-	
-	
 }
