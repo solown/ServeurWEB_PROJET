569a436c5eec4ee1951e56c775f97665f20e5a1f
Alice <alice.capelle@sfr.fr>
2018-03-29 18:07:58 +0200

    Fix error in swipe js

--- site/scripts/swipe.js
+++ site/scripts/swipe.js
@@ -1,16 +1,14 @@
 const yes = document.querySelector(".yes");
 const swipe_profil = document.querySelector(".swipe_profil");
 const no = document.querySelector(".no");
-var php_student = <?php echo json_encode($tab_student); ?>;
-var tab_student = JSON.parse(student);
-console.log(tab_student);
+
 
 var recycled = document.querySelector(".bounceOutLeft");
 var hearted = document.querySelector(".bounceOutRight");
 
 yes.addEventListener("click", () => {
 	swipe_profil.classList.add("bounceOutRight");
-	//document.getElementbyId("swipe_name").innerHTML = 
+
 });
 
 no.addEventListener("click", () => {
