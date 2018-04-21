b0633ca29a7d30f0aa73b6d324d7911ea3b75ab3
Alice Capelle <capelle@localhost.localdomain>
2018-04-21 15:28:58 +0200

    Add log for debug

--- site/scripts/sign_up.js
+++ site/scripts/sign_up.js
@@ -1,9 +1,11 @@
 function sign_up() {
+	console.log("debut de sign_up.js");
 	var xhttp = new XMLHttpRequest();
-
+	
 	xhttp.onreadystatechange = function(){
 		if(this.readyState == 4 && this.status == 200){
 			if(this.responseText == "OK"){
+				console.log("ok");
 				highlight(document.getElementsByName("mail")[0], false);
 				highlight(document.getElementsByName("password")[0], false);
 				xhttp.open("POST", "../controller/register-confirmation.php", true);
@@ -17,6 +19,7 @@ function sign_up() {
 				
 			}
 			else {
+				console.log("mail existe deja");
 				highlight(document.getElementsByName("mail")[0], true);
 				highlight(document.getElementsByName("password")[0], true);
 			}
@@ -29,5 +32,6 @@ function sign_up() {
 	var mail = document.getElementsByName("mail")[0].value;
 
 	xhttp.send("mail=" + mail);
+	console.log("end");
 	return false;
 }
