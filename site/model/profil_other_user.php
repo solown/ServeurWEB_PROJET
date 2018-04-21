f1f90c3becb351edb63cfdf8018610e1170335aa
joujoucous <coustenoble.julie@gmail.com>
2018-04-21 17:56:41 +0200

    new hope

--- site/model/profil_other_user.php
+++ site/model/profil_other_user.php
@@ -8,7 +8,7 @@ $student;
 if($db) {
 	echo $student_mail;
 	$query = "SELECT S.year, S.surname, S.email, S.pic, S.description, A.wording as adj1, A2.wording as adj2, A3.wording as adj3 
-	FROM ADJECTIVE A, ADJECTIVE A2, ADJECTIVE A3, STUDENT S WHERE S.email = `:mail` AND S.adjective_1 = A.id_adjective AND S.adjective_2 = A2.id_adjective AND S.adjective_3 = A3.id_adjective";
+	FROM ADJECTIVE A, ADJECTIVE A2, ADJECTIVE A3, STUDENT S WHERE S.email = :mail AND S.adjective_1 = A.id_adjective AND S.adjective_2 = A2.id_adjective AND S.adjective_3 = A3.id_adjective";
 	$statement = $db->prepare($query);
 	$statement->bindValue(':mail', $student_mail);
 	$statement->execute();
