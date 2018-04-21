a8319214842e664fad411cfe1c17fa3335ebccbf
Alice Capelle <capelle@localhost.localdomain>
2018-04-21 16:11:52 +0200

    Add var_dump in signup php

--- site/controller/sign_up.php
+++ site/controller/sign_up.php
@@ -4,11 +4,10 @@
 	
 	
 	$student_mail =	 $_POST['mail'];
-	var_dump($student_mail);
+
 	require("../model/sign_up.php");
 
-	if($row == 0) {
-	var_dump($row);
+	if($statement->rowCount() == 0) {
 		echo "OK";
 	}
 	else
