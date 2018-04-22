99d8c3171a1611bc5c21b36287cb19c2b11d299c
Alice Capelle <capelle@localhost.localdomain>
2018-04-22 18:33:03 +0200

    Only display profil from other year

--- site/model/swipe.php
+++ site/model/swipe.php
@@ -8,7 +8,7 @@ function getArrayStudents() {
 	$tab_student = array();
 	if($db) {
 
-		$query_get_score = "SELECT score 
+		$query_get_score = "SELECT score, year  
 			FROM student
 			WHERE id_student = :student_id";
 		$statement_score = $db->prepare($query_get_score);
@@ -17,15 +17,25 @@ function getArrayStudents() {
 
 		while($row = $statement_score->fetch(PDO::FETCH_ASSOC)){
 			$score_student = $row['score'];
+			$student_year = $row['year'];
 		}
 
 		$score_min = $score_student - 50;
 		$score_max = $score_student + 50;
 
-		$query_get_student = 
+		if($student_year == 1){
+			$query_get_student = 
 			"SELECT A.wording as adj1, A2.wording as adj2, A3.wording as adj3, S.surname, S.description, S.pic, S.email
 			FROM ADJECTIVE A, ADJECTIVE A2, ADJECTIVE A3, STUDENT S
-			WHERE S.id_student <> :student_id AND S.score BETWEEN :score_min AND :score_max AND S.adjective_1 = A.id_adjective AND S.adjective_2 = A2.id_adjective AND S.adjective_3 = A3.id_adjective"; 
+			WHERE S.id_student <> :student_id AND year = 2 AND S.score BETWEEN :score_min AND :score_max AND S.adjective_1 = 				A.id_adjective AND S.adjective_2 = A2.id_adjective AND S.adjective_3 = A3.id_adjective"; 
+		}
+		else {
+			$query_get_student = 
+			"SELECT A.wording as adj1, A2.wording as adj2, A3.wording as adj3, S.surname, S.description, S.pic, S.email
+			FROM ADJECTIVE A, ADJECTIVE A2, ADJECTIVE A3, STUDENT S
+			WHERE S.id_student <> :student_id AND year = 1 AND S.score BETWEEN :score_min AND :score_max AND S.adjective_1 = 				A.id_adjective AND S.adjective_2 = A2.id_adjective AND S.adjective_3 = A3.id_adjective"; 
+		}
+			
 		$statement_student = $db->prepare($query_get_student);
 		$statement_student->bindValue(':score_min', $score_min, PDO::PARAM_INT);
 		$statement_student->bindValue(':score_max', $score_max, PDO::PARAM_INT);
