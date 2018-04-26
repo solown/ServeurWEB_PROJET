0b7d1b15380f4669e2af7be4317e8ff645b63eb6
Alice Capelle <capelle@localhost.localdomain>
2018-04-26 21:39:10 +0200

    Debug

--- site/controller/like_student.php
+++ site/controller/like_student.php
@@ -9,10 +9,11 @@ if($db) {
 		
 	//Id of the two student. One come from the session, we get the other one from a post method of the swipe page
 	$id_student_connected = $_SESSION['id'];
-	if (isset($_POST['mail_student_liked'])){
-		$mail_student_liked = $_POST['mail_student_liked'];
-	}
-	
+
+	$mail_student_liked = $_POST['mail_student_liked'];
+	var_dump($_POST['mail_student_liked']);
+	var_dump($mail_student_liked);
+
 	//We build a student object. That's the student curently connected
 	$query_get_student_connected = "SELECT S.year
 	FROM student S WHERE S.id_student = :id_student_connected";
