b960d94f30409da70794cebdc87e2f3cbc60c684
Alice Capelle <capelle@localhost.localdomain>
2018-04-28 12:37:59 +0200

    Missing (

--- site/model/score.php
+++ site/model/score.php
@@ -1,22 +1,23 @@
 <?php
 
-	$ratio = ((abs($student_connected_score - $student_liked_score))**2)/100
-	if($student_connected_score>$student_liked_score):
+	$ratio = ((abs($student_connected_score - $student_liked_score)**2)/100);
+	if($student_connected_score>$student_liked_score){
 		$student_connected_score = $student_connected_score-$ratio;
 		$student_liked_score = $student_liked_score+$ratio;
-	else:
+	}
+	else{
 		$student_connected_score = $student_connected_score+$ratio;
 		$student_liked_score = $student_liked_score-$ratio;
-		
+	}
 	$query_update_score1 = "UPDATE STUDENT score = :score1
-	WHERE id_student = :id_s1"
+	WHERE id_student = :id_s1";
 	$statement_update_score1 = $db->prepare($query_update_score1);
 	$statement_update_score1->bindValue(':id_s1' $id_student_connected); 
 	$statement_update_score1->bindValue(':score1', $student_connected_score); 
 	$statement_update_score1->execute();
 	
 	$query_update_score2 = "UPDATE STUDENT score = :score2
-	WHERE id_student = :id_s2"
+	WHERE id_student = :id_s2";
 	$statement_update_score2 = $db->prepare($query_update_score2);
 	$statement_update_score2->bindValue(':id_s2', $id_student_liked); 
 	$statement_update_score2->bindValue(':score2', $student_liked_score); 
