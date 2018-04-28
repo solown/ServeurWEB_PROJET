332d6a868ef2ec27a98ca71b5302f67c9d5afa87
Alice Capelle <capelle@localhost.localdomain>
2018-04-28 18:23:13 +0200

    Ceil for score values

--- site/model/score.php
+++ site/model/score.php
@@ -1,7 +1,6 @@
 <?php
 
 	$ratio = ((abs($student_connected_score - $student_liked_score)**2)/100);
-	ceil($ratio);
 	if($student_connected_score>$student_liked_score){
 		$student_connected_score = $student_connected_score-$ratio;
 		$student_liked_score = $student_liked_score+$ratio;
@@ -14,14 +13,14 @@
 	WHERE id_student = :id_s1";
 	$statement_update_score1 = $db->prepare($query_update_score1);
 	$statement_update_score1->bindValue(':id_s1', $id_student_connected); 
-	$statement_update_score1->bindValue(':score1', $student_connected_score); 
+	$statement_update_score1->bindValue(':score1', ceil($student_connected_score)); 
 	$statement_update_score1->execute();
 	
 	$query_update_score2 = "UPDATE student SET score = :score2
 	WHERE id_student = :id_s2";
 	$statement_update_score2 = $db->prepare($query_update_score2);
 	$statement_update_score2->bindValue(':id_s2', $id_student_liked); 
-	$statement_update_score2->bindValue(':score2', $student_liked_score); 
+	$statement_update_score2->bindValue(':score2', ceil($student_liked_score)); 
 	$statement_update_score2->execute();
 
 
