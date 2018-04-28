02dc4a614de079231f2d7bdfe75b2001c1f63714
Alice Capelle <capelle@localhost.localdomain>
2018-04-28 12:41:59 +0200

    Syntaxe errors

--- site/model/score.php
+++ site/model/score.php
@@ -9,14 +9,14 @@
 		$student_connected_score = $student_connected_score+$ratio;
 		$student_liked_score = $student_liked_score-$ratio;
 	}
-	$query_update_score1 = "UPDATE STUDENT score = :score1
+	$query_update_score1 = "UPDATE student SET score = :score1
 	WHERE id_student = :id_s1";
 	$statement_update_score1 = $db->prepare($query_update_score1);
 	$statement_update_score1->bindValue(':id_s1', $id_student_connected); 
 	$statement_update_score1->bindValue(':score1', $student_connected_score); 
 	$statement_update_score1->execute();
 	
-	$query_update_score2 = "UPDATE STUDENT score = :score2
+	$query_update_score2 = "UPDATE student SET score = :score2
 	WHERE id_student = :id_s2";
 	$statement_update_score2 = $db->prepare($query_update_score2);
 	$statement_update_score2->bindValue(':id_s2', $id_student_liked); 
