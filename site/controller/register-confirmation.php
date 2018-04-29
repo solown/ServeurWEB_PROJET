5f24a6a206531f00100187919c0e71d75d2d9d00
Alice Capelle <capelle@localhost.localdomain>
2018-04-29 19:16:18 +0200

    Correct html error in mail

--- site/controller/register-confirmation.php
+++ site/controller/register-confirmation.php
@@ -32,75 +32,73 @@ $message = (new Swift_Message("Registration confirmation"))
 	->setFrom(["find.the.r8.one@gmail.com" => "Find the right one"])
 	->setTo([$student_mail."@etu.parisdescartes.fr" => $student_name])
 
-	->setBody("
-	<!DOCTYPE html>
-	<html xmlns:v='urn:schemas-microsoft-com:vml'>
-	<head>
-	    <meta http-equiv='content-type' content='text/html; charset=utf-8'>
-	    <meta name='viewport' content='width=device-width; initial-scale=1.0; maximum-scale=1.0;'>
-	    <link href='https://fonts.googleapis.com/css?family=Questrial' rel='stylesheet' type='text/css'>
-	    <link href='https://fonts.googleapis.com/css?family=Fjalla+One' rel='stylesheet'>
-	</head>
-	<body leftmargin='0' topmargin='0' marginwidth='0' marginheight='0' style='font-family: Fjalla One;'>
+	->setBody(
+		'<!DOCTYPE html>'.
+		'<html xmlns:v="urn:schemas-microsoft-com:vml">'.
+		'<head>'.
+		    '<meta http-equiv="content-type" content="text/html; charset=utf-8">'.
+		    '<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">'.
+		    '<link href="https://fonts.googleapis.com/css?family=Questrial" rel="stylesheet" type="text/css">'.
+		    '<link href="https://fonts.googleapis.com/css?family=Fjalla+One" rel="stylesheet">'.
+		'</head>'.
+		'<body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" style="font-family: Fjalla One;">'.
 
-	<table bgcolor='#F5F7FA' width='100%' border='0' cellpadding='0' cellspacing='0'>
-	    <tbody>
-	    	<tr style='color : #707070; font-size:20px'>
-			<td align='center'>
-				Bienvenue <span style='color : #61B8D0'>'.$student_name.'</span>,
-			</td>
-	    	</tr>
-		<tr style='color : #707070; font-size:20px'>
-			<td align='center'>
-				pour valider ton compte clique sur le lien suivant :
-			</td>
-		</tr>
-		<tr>
-			<td>
-				&nbsp;
-			</td>
-		</tr>
-		<tr>
-			<td align='center'>
-				<a href='http://tinder.student.elwinar.com/view/loginPerso.php?token='.$token_hash.'&name='.$student_name.'' style='color : #61B8D0; font-size:20px'>http://tinder.student.elwinar.com/view/loginPerso.php?token='.$token_hash.'&name='.$student_name.'</a>
-			</td>
-		</tr>
-		<tr>
-			<td align='center'>
-				&nbsp;
-			</td>
-		</tr>
-		<tr>
-			<td align='center'>
-				<img src='https://zupimages.net/up/18/17/m23z.png' alt='Logo' width='500px' height='344px'/>
-			</td>
-		</tr>
-		<tr>
-			<td>
-				&nbsp;
-			</td>
-		</tr>
-		<tr style='color : #707070'>
-			<td align='center'>
-				Pense à la planète, après avoir validé ton compte, supprime ce mail.
-			</td>
-		</tr>
-		<tr style='color : #707070'>
-			<td align='center'>
-				Le stockage de mail fait tourner suotidiennement l'équivalent de
-			</td>
-		</tr>
-		<tr style='color : #707070'>
-			<td align='center'>
-				quatre centrales nucléaire dans le monde.
-			</td>
-		</tr>
-	    </tbody>
-	</table>
-
-	</html>
-
-", "texte/html")
+		'<table bgcolor="#F5F7FA" width="100%" border="0" cellpadding="0" cellspacing="0">'.
+		    '<tbody>'.
+		    	'<tr style="color : #707070; font-size:20px">'.
+			'	<td align="center">'.
+					'Bienvenue <span style="color : #61B8D0">".$student_name."</span>,'.
+				'</td>'.
+		    	'</tr>'.
+		'	<tr style="color : #707070; font-size:20px">'.
+		'		<td align="center">'.
+		'			pour valider ton compte clique sur le lien suivant :'.
+		'		</td>'.
+		'	</tr>'.
+				'<td>'.
+				'	<tr>'.
+				'	&nbsp;'.
+				'</td>'.
+		'	<tr>'.
+		'	</tr>'.
+		'		<td align="center">'.
+		'			<a href="http://tinder.student.elwinar.com/view/loginPerso.php?token=".$token_hash."&name=".$student_name."" style="color : #61B8D0; font-size:20px">http://tinder.student.elwinar.com/view/loginPerso.php?token=".$token_hash."&name=".$student_name."</a>'.
+		'		</td>'.
+		'	</tr>'.
+		'	<tr>'.
+		'		<td align="center">'.
+			'	</td>'.
+				'			&nbsp;'.
+		'	</tr>'.
+		'	<tr>'.
+		'		<td align="center">'.
+		'			<img src="https://zupimages.net/up/18/17/m23z.png" alt="Logo" width="500px" height="344px"/>'.
+		'		</td>'.
+		'	</tr>'.
+		'	<tr>'.
+		'		<td>'.
+		'			&nbsp;'.
+		'		</td>'.
+			'<tr style="color : #707070">'.
+			'	</tr>'.
+			'	<td align="center">'.
+			'		Pense à la planète, après avoir validé ton compte, supprime ce mail.'.
+			'	</td>'.
+			'</tr>'.
+			'<tr style="color : #707070">'.
+			'	<td align="center">'.
+			'		Le stockage de mail fait tourner suotidiennement l"équivalent de'.
+			'	</td>'.
+		'	</tr>'.
+		'	<tr style="color : #707070">'.
+		'		<td align="center">'.
+		'			quatre centrales nucléaire dans le monde.'.
+		'		</td>'.
+		  '  </tbody>'.
+				'	</tr>'.
+'		</table>'.
+'		</html>'.
+	, "texte/html")
 
 //	->setBody("Please, confirm your registration by clicking on the following link : http://tinder.student.elwinar.com/view/loginPerso.php?token=".$token_hash."&name=".$student_name."\n")
 //	->setBody("Please, confirm your registration by clicking on the following link : " . $_SERVER['SERVER_NAME'] . "/view/loginPerso.php?token=".$token_hash."&name=".$student_name."\n")
