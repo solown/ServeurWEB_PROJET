3c0d3fd1e3a6eebca23c2c6003297c284a0c49ad
Alice Capelle <capelle@localhost.localdomain>
2018-04-30 15:42:54 +0200

    Front of the email forget password

--- site/controller/forgot_passwd.php
+++ site/controller/forgot_passwd.php
@@ -20,12 +20,78 @@ $mailer = new Swift_Mailer($transport);
 
 $root = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/';
 
+$psw_link = 'http://tinder.student.elwinar.com/view/change_passwd.php?mail='.$student_mail.'&token='.$token_hash;
+
 $message = (new Swift_Message("Change your password"))
 	->setFrom(["find.the.r8.one@gmail.com" => "Find the right one"])
 	->setTo([$student_mail."@etu.parisdescartes.fr" => $student_name])
-	->setBody("You asked to change your password.\n
-Please, click on the following link to reset your password : http://tinder.student.elwinar.com/view/change_passwd.php?mail=".$student_mail."&token=".$token_hash)
-;
+	->setBody(
+		'<!DOCTYPE html>'.
+		  '<html xmlns:v="urn:schemas-microsoft-com:vml">'.
+		  '<head>'.
+		      '<meta http-equiv="content-type" content="text/html; charset=utf-8">'.
+		      '<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">'.
+		      '<link href="https://fonts.googleapis.com/css?family=Questrial" rel="stylesheet" type="text/css">'.
+		      '<link href="https://fonts.googleapis.com/css?family=Fjalla+One" rel="stylesheet">'.
+		  '</head>'.
+		  '<body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" style="font-family: Fjalla One;">'.
+
+		  '<table bgcolor="#F5F7FA" width="100%" border="0" cellpadding="0" cellspacing="0">'.
+		      '<tbody>'.
+		        '<tr style="color : #707070; font-size:20px">'.
+		    '	<td align="center">'.
+		        'Bienvenue <span style="color : #FF5544">'.$student_name.'</span>,'.
+		      '</td>'.
+		        '</tr>'.
+		  '	<tr style="color : #707070; font-size:20px">'.
+		  '		<td align="center">'.
+		  '			pour réinitialiser ton mot de passe clique sur le lien suivant :'.
+		  '		</td>'.
+		  '	</tr>'.
+		      '<td>'.
+		      '	<tr>'.
+		      '	&nbsp;'.
+		      '</td>'.
+		  '	<tr>'.
+		  '	</tr>'.
+		  '		<td align="center">'.
+		  '			<a href="'.$psw_link.'" style="color : #FF5544; font-size:20px">'.$psw_link.'</a>'.
+		  '		</td>'.
+		  '	</tr>'.
+		  '	<tr>'.
+		  '		<td align="center">'.
+		    '	</td>'.
+		      '			&nbsp;'.
+		  '	</tr>'.
+		  '	<tr>'.
+		  '		<td align="center">'.
+		  '			<img src="https://zupimages.net/up/18/17/m23z.png" alt="Logo" width="500px" height="344px"/>'.
+		  '		</td>'.
+		  '	</tr>'.
+		  '	<tr>'.
+		  '		<td>'.
+		  '			&nbsp;'.
+		  '		</td>'.
+		    '<tr style="color : #707070">'.
+		    '	</tr>'.
+		    '	<td align="center">'.
+		    '		Pense à la planète, après avoir validé ton compte, supprime ce mail.'.
+		    '	</td>'.
+		    '</tr>'.
+		    '<tr style="color : #707070">'.
+		    '	<td align="center">'.
+		    '		Le stockage de mail fait tourner quotidiennement l équivalent de'.
+		    '	</td>'.
+		  '	</tr>'.
+		      '<tr style="color : #707070">'.
+		  '		<td align="center">'.
+		  '			quatre centrales nucléaire dans le monde.'.
+		  '		</td>'.
+		    '  </tbody>'.
+		      '	</tr>'.
+		'		</table>'.
+		'		</html>',"text/html"
+	);
 
 $result = $mailer->send($message);
 
