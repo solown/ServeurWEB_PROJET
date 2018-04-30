060ae1d73382d3ce6d99f24980ce1fe8f6770369
joujoucous <coustenoble.julie@gmail.com>
2018-04-30 21:34:33 +0200

    update profile description :sparkles:

--- site/scripts/updateprofile.js
+++ site/scripts/updateprofile.js
@@ -1,25 +1,32 @@
+const edit = document.querySelector('.edit')
+const confirm = document.querySelector('.confirm')
+const cancel = document.querySelector('.cancel')
+const newDescription = document.querySelector('.new_description')
+const oldDescription = document.querySelector('.old_description')
+const form = document.querySelector('#changeDescription')
 
 
-function update(){
-	document.getElementsByClassName('resume')[0].style.display = 'none';
-	document.getElementById('inputresume').style.display = 'block';
-	document.getElementsByClassName('buttonupdate')[0].style.display = 'none';
-	document.getElementsByClassName('buttonconfirm')[0].style.display = 'block';
-	document.getElementById('picstudent').style.display = 'none';
-	document.getElementsByClassName('input_btn')[0].style.display = 'block';
-}
-function cancel(){
-	document.getElementsByClassName('resume')[0].style.display = 'block';
-	document.getElementById('inputresume').style.display = 'none';
-	document.getElementsByClassName('buttonupdate')[0].style.display = 'block';
-	document.getElementsByClassName('buttonconfirm')[0].style.display = 'none';
-	document.getElementById('picstudent').style.display = 'block';
-	document.getElementsByClassName('input_btn')[0].style.display = 'none';
-}
+edit.addEventListener('click', () => {
+	edit.style.display = 'none'
+	cancel.style.display = 'inline-block'
+	confirm.style.display = 'inline-block'
+	oldDescription.style.display = 'none'
+	newDescription.style.display = 'block'
+})
+confirm.addEventListener('click', () => {
+	edit.style.display = 'block'
+	cancel.style.display = 'none'
+	confirm.style.display = 'none'
+	oldDescription.style.display = 'block'
+	newDescription.style.display = 'none'
+	form.submit();
+})
+cancel.addEventListener('click', () => {
+	edit.style.display = 'block'
+	cancel.style.display = 'none'
+	confirm.style.display = 'none'
+	oldDescription.style.display = 'block'
+	newDescription.style.display = 'none'
+	newDescription.value = ''
 
-
-
-
-
-
-
+})
--- site/view/updateprofile.php
+++ site/view/updateprofile.php
@@ -4,65 +4,63 @@ if(!isset($_SESSION['id']))
 {
     header('Location: ../view/logout.php');
 }
-?>
+
+require("../model/updateprofile.php"); ?>
 	<!DOCTYPE html>
 	<html lang="fr">
 
 	<head>
 		<meta charset="utf-8" />
-		<title>Update profile</title>
+		<title>Profil Other User</title>
 		<!--		<CSS>				-->
 		<link rel="stylesheet" href="../styles/main.css">
 		<link rel="stylesheet" href="../styles/signup_login.css">
 		<!--		<font>				-->
 		<link href="https://fonts.googleapis.com/css?family=Fjalla+One" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
-
 		<!--		<bootstrap>				-->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	</head>
 
 	<body>
-		<?php include('../model/updateprofile.php') ?>
 		<div class="menu">
 			<a href="../view/swipe.php" class="menu_inactive">swipe</a>
 			<a href="../view/updateprofile.php" class="menu_inactive">my account</a>
 			<a href="#" class="menu_inactive">messages</a>
 			<a href="../view/logout.php" class="menu_inactive">log out</a>
 		</div>
-		<form method="post">
-			<div class="back"></div>
-			<div class="picture_profile img_profile"><img src="<?php echo htmlspecialchars($picstudent); ?> " alt=""></div>
-			<div class="cloud_profile"><img src="../images/cloud.svg" alt=""></div>
-			<div class="year_email_profile">
-				<span class="DUT">DUT
-				<?php echo htmlspecialchars($yearstudent); ?> -</span>
+		<div class="back"></div>
+		<div class="picture_profile img_profile"><img src="<?php echo htmlspecialchars($student->getPic()); ?> " alt=""></div>
+		<div class="cloud_profile"><img src="../images/cloud.svg" alt=""></div>
+		<div class="year_email_profile">
+			<span class="DUT">DUT
+			<?php echo($student->getYear()); ?> -</span>
 
-				<?php echo htmlspecialchars($mailstudent); ?>@etu.parisdescartes.fr</div>
-			<div class="stats_profile">
-				<div><?php echo htmlspecialchars($match); ?> matchs</div>
-				<div>0 parainage</div>
-			</div>
-			<div class="name_profile">
-				<?php echo htmlspecialchars($name); ?>
-			</div>
-			<div class="adj_profile">
-				<?php echo htmlspecialchars($word1); ?>-<?php echo htmlspecialchars($word2); ?>-<?php echo htmlspecialchars($word3); ?>
-			</div>
-			<div class="description_profile">
-				<?php echo htmlspecialchars($description); ?>
-				<input id="inputresume" name="resumestudent" placeholder="Décris toi ici, n'hésite pas à dire ce que tu aimes ou ce que tu cherches sur notre app :)" type="textarea"
-					maxlength="280" >
-				</input>
-				<div class="buttonupdate" onclick="update()">
-				</div>
-				<input class="buttonconfirm" name="updatedescribe" onclick="confirm()" type="submit" value="">
-				</input>
-				<input class="cancelupdate" name="N_cancelupdate" onclick="cancel()" value=""></input>
-			</div>
+			<?php echo($student->getEmail()); ?>@etu.parisdescartes.fr</div>
+		<div class="stats_profile">
+			<div>
+				<?php echo($match); ?> matchs</div>
+			<div>0 parainage</div>
+		</div>
+		<div class="name_profile">
+			<?php echo($student->getSurname()); ?>
+		</div>
+		<div class="adj_profile">
+			<?php echo($student->getStringAdjectives()); ?>
+		</div>
+		<div class="description_profile old_description">
+			<?php echo htmlspecialchars($student->getDescription()); ?>
+		</div>
+		<form method="post" id="changeDescription" action="#">
+			<textarea class="description_profile new_description" name="description" rows="4" cols="7" maxlength="280"><?php echo htmlspecialchars($student->getDescription()); ?></textarea>
 		</form>
+		<div class="edit_profile">
+			<img class="edit" src="../images/edit.png" alt="edit">
+			<img class="confirm" src="../images/checked.png" alt="edit">
+			<img class="cancel" src="../images/cancel-button-3.png" alt="edit">
+		</div>
+
 		<script src="../scripts/updateprofile.js"></script>
-		</form>
 	</body>
 
 	</html>
