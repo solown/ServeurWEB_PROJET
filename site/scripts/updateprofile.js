5c0f6667c94f75955a079255750d88fff20bca72
unknown <thibaudrider@live.fr>
2018-04-30 19:51:58 +0200

    Js update

--- site/scripts/updateprofile.js
+++ site/scripts/updateprofile.js
@@ -5,16 +5,16 @@ function update(){
 	document.getElementById('inputresume').style.display = 'block';
 	document.getElementsByClassName('buttonupdate')[0].style.display = 'none';
 	document.getElementsByClassName('buttonconfirm')[0].style.display = 'block';
-	document.getElementById('picstudent').style.display = 'none';
-	document.getElementsByClassName('input_btn')[0].style.display = 'block';
+	/*document.getElementById('picstudent').style.display = 'none';
+	document.getElementsByClassName('input_btn')[0].style.display = 'block';*/
 }
 function cancel(){
-	document.getElementsByClassName('resume')[0].style.display = 'block';
+	document.getElementsByClassName('description_profile')[0].style.display = 'block';
 	document.getElementById('inputresume').style.display = 'none';
 	document.getElementsByClassName('buttonupdate')[0].style.display = 'block';
 	document.getElementsByClassName('buttonconfirm')[0].style.display = 'none';
-	document.getElementById('picstudent').style.display = 'block';
-	document.getElementsByClassName('input_btn')[0].style.display = 'none';
+	/*document.getElementById('picstudent').style.display = 'block';
+	document.getElementsByClassName('input_btn')[0].style.display = 'none';*/
 }
 
 
