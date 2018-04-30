a5ebe40a5d8933f1a74d9ae15f4c94e2fdaeb4ca
unknown <thibaudrider@live.fr>
2018-04-30 19:10:57 +0200

    Style again

--- site/view/updateprofile.php
+++ site/view/updateprofile.php
@@ -5,66 +5,51 @@ if(!isset($_SESSION['id']))
     header('Location: ../view/logout.php');
 }
 ?>
-	<!DOCTYPE HTML>
+	<!DOCTYPE html>
 	<html lang="fr">
 
-	<header>
+	<head>
 		<meta charset="utf-8" />
-		<title>Update Profile</title>
+		<title>Update profile</title>
+		<!--		<CSS>				-->
 		<link rel="stylesheet" href="../styles/main.css">
-		<link rel="stylesheet" href="../styles/updateprofile.css">
+		<link rel="stylesheet" href="../styles/signup_login.css">
+		<!--		<font>				-->
 		<link href="https://fonts.googleapis.com/css?family=Fjalla+One" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
-	</header>
+
+		<!--		<bootstrap>				-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	</head>
 
 	<body>
-		<?php include('../model/updateprofile.php') ?>
 		<div class="menu">
 			<a href="../view/swipe.php" class="menu_inactive">swipe</a>
-			<a href="#" class="menu_active">my account</a>
+			<a href="../view/updateprofile.php" class="menu_inactive">my account</a>
 			<a href="#" class="menu_inactive">messages</a>
 			<a href="../view/logout.php" class="menu_inactive">log out</a>
 		</div>
-		<div id="mailclasse">
-			<p><b>DUT <?php echo htmlspecialchars($yearstudent)?></b></p><span><?php echo htmlspecialchars($mailstudent) ?>@etu.parisdescartes.fr</span>
+		<div class="back"></div>
+		<div class="picture_profile img_profile"><img src="<?php echo htmlspecialchars($picstudent); ?> " alt=""></div>
+		<div class="cloud_profile"><img src="../images/cloud.svg" alt=""></div>
+		<div class="year_email_profile">
+			<span class="DUT">DUT
+			<?php echo htmlspecialchars($yearstudent); ?> -</span>
+
+			<?php echo htmlspecialchars($mailstudent); ?>@etu.parisdescartes.fr</div>
+		<div class="stats_profile">
+			<div><?php echo htmlspecialchars($match); ?> matchs</div>
+			<div>0 parainage</div>
 		</div>
-		<div id="stats">
-			<h2>
-				<?php echo htmlspecialchars($match)?> matchs</h2>
-			<h2> 0 filleule </h2>
+		<div class="name_profile">
+			<?php echo htmlspecialchars($name); ?>
 		</div>
-		<div id="present">
-				<?php echo '<img id="picstudent" src="'.$picstudent.'" alt=""/>'?> 
-				<input class="input_btn" name="upload_pic" type="file" >
-				</input>
-			<div id="background">
-			</div>
-			<div class="name">
-				<center>
-					<?php echo htmlspecialchars($name) ?> </center>
-			</div>
-			<div class="adj">
-				<center>
-					<?php echo htmlspecialchars($word1) ?>-
-					<?php echo htmlspecialchars($word2) ?>-
-					<?php echo htmlspecialchars($word3) ?>
-				</center>
-			</div>
-			<form method="post">
-				<div class="resume">
-					<?php echo htmlspecialchars($description)?>
-				</div>
-				<input id="inputresume" name="resumestudent" placeholder="Décris toi ici, n'hésite pas à dire ce que tu aimes ou ce que tu cherches sur notre app :)" type="textarea"
-					maxlength="280" >
-				</input>
+		<div class="adj_profile">
+			<?php echo htmlspecialchars($word1); ?>-<?php echo htmlspecialchars($word2); ?>-<?php echo htmlspecialchars($word3); ?>
 		</div>
-		<div class="buttonupdate" onclick="update()">
+		<div class="description_profile">
+			<?php echo htmlspecialchars($description); ?>
 		</div>
-		<input class="buttonconfirm" name="updatedescribe" onclick="confirm()" type="submit" value="">
-		</input>
-		<input class="cancelupdate" name="N_cancelupdate" onclick="cancel()" value=""></input>
-		</form>
-		<script src="../scripts/updateprofile.js"></script>
 	</body>
 
 	</html>
